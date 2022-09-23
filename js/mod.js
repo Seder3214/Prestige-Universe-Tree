f68ec99d95a45190dffb116f3286c26164b50345
Seder3214 <50592797+Seder3214@users.noreply.github.com>
2022-09-23 19:29:31 +0300

    v1.02 revert

--- js/mod.js
+++ js/mod.js
@@ -43,7 +43,7 @@ function getPointGen() {
 		return new Decimal(0)
 
 	let gain = new Decimal(1)
-	if (hasMilestone("mp", 11)) return gain = gain.add(1).pow(player.mp.points.times(5))
+	if (hasMilestone("mp", 11)) return gain = gain.add(1).pow(player.mp.points)
 	if (hasUpgrade("p", 11)) gain = gain.times(2)
 	if (hasUpgrade("p", 12)) gain = gain.times(upgradeEffect("p", 12))
 	if (hasUpgrade("p", 22)) gain = gain.times(upgradeEffect("p", 12).div(3.5))
